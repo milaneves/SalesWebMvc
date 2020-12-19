38de79190867d9cdf16f78c4a07b31317a125574
Mila Neves <mila.neves000@gmail.com>
2020-12-19 09:17:17 -0300

    Criacao de entidades , inserindo dados no banco

---
+++ SalesWebMvc/Data/SeedingService.cs
@@ -0,0 +1,37 @@
+﻿using SalesWebMvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc.Data
+{
+    public class SeedingService
+    {
+
+        private SalesWebMvcContext _context;
+        public SeedingService(SalesWebMvcContext context)
+        {
+            _context = context;
+        }
+
+        //populando a base de dados
+        public void Seed()
+        {
+            if ( _context.Department.Any() ||
+                 _context.Seller.Any() ||
+                _context.SalesRecord.Any()
+                )
+              {
+                return; //banco já foi populado
+              }
+
+            //Department d1 = new Department("Mila");
+
+            //usando o enity para adcionar no banco e confirmando o salvamento
+            //_context.Department.AddRange(d1);
+            //_context.SaveChanges();
+
+        }
+    }
+}
---
+++ SalesWebMvc/Services/SellerService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SalesWebMvc.Data;
+using SalesWebMvc.Models;
+
+namespace SalesWebMvc.Services
+{
+    public class SellerService
+    {
+         
+        private readonly SalesWebMvcContext _context;
+
+        public SellerService(SalesWebMvcContext context)
+        {
+            _context = context;
+        }
+
+        //retorna lista com os vendedores do banco
+        public List<Seller> FindAll()
+        {
+            return _context.Seller.ToList();
+        }
+       
+    }
+}
